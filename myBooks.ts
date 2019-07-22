d06aab1545da8cf1474e7a8e7de51ff2adb2f3bc
vrajbhagat <52467355+vrajbhagat@users.noreply.github.com>
2019-07-22 12:13:36 -0400

    Delete myBooks.ts

--- myBooks.ts
+++
@@ -1,7 +0,0 @@
-import {BookInfo} from '../../app/bookInfo';
-export const MYBOOKS: BookInfo[] = [
-    {bname: 'Aurora Rising', bauthor: 'Amie Kaufman', bgenre: "Children's", byear: '07-05-2019', bpicture:"image1"},
-    {bname: 'The October Man ', bauthor: 'Ben Aaronovitch', bgenre: "Fantasy", byear: '01-05-2019', bpicture:"image2"},
-    {bname: 'The Paper Wasp', bauthor: 'Lauren Acampora', bgenre: "Fiction", byear: '01-06-2019', bpicture:"image3"},
-    {bname: 'The Redemption of Time', bauthor: 'Baoshu', bgenre: "Horror", byear: '16-07-2019', bpicture:"image4"}
-]
